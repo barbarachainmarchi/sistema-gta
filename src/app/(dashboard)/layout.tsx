25a45f93ee1eaa47be22641894013d754d333385
Barbara <barbarachainmarchi@gmail.com>
2026-03-22 20:00:55 -0300

    investigacao

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,6 +4,8 @@ import { Sidebar } from '@/components/layout/sidebar'
 import { ThemeProvider } from '@/components/layout/theme-provider'
 import { getTema } from '@/lib/getTema'
 
+export const dynamic = 'force-dynamic'
+
 export default async function DashboardLayout({
   children,
 }: {
